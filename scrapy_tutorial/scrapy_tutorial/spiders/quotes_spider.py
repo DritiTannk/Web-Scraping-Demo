4450f01b9dea3465a3aaf73d397944e751fce987
Driti Tannk <driti.tannk@thegatewaycorp.com>
2020-11-23 16:02:15 +0530

    Scrape Multiple Link Pages

--- scrapy_tutorial/scrapy_tutorial/spiders/quotes_spider.py
+++ scrapy_tutorial/scrapy_tutorial/spiders/quotes_spider.py
@@ -4,21 +4,24 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes_spider"
     start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
+        'http://quotes.toscrape.com/',
     ]
 
     def parse(self, response):
         quotes_list = response.css('div.quote')
         for quote in quotes_list:
-            text = quote.css("span.text::text").get()
-            author = quote.css("small.author::text").get()
-            tags = quote.css("div.tags a.tag").getall()
+            text = quote.css("span.text::text").get()  # Extracts quote
+            author = quote.css("small.author::text").get()  # Extracts author
+            tags = quote.css("div.tags a.tag").getall()  # Extracts tags associated with it
             page_no = response.url.split('/')[-2]
             yield {
                 'text': text,
                 'author': author,
                 'tags': tags,
-                'page_no': page_no,
+                'page_no': page_no
             }
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)  # Creates a new url by appending the page number.
+            yield scrapy.Request(next_page, callback=self.parse)
 
